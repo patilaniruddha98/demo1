76a3e0bafbca72b5724d1a5d39279add44305588
Aniruddha Patil <patilaniruddha98@gmail.com>
2023-05-07 12:27:34 +0530

    second commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the container
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code to the container
+COPY . .
+
+
+CMD ["npm","start"]
---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import productRouter from './productRouter.js';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: '*'
+}));
+
+// eslint-disable-next-line no-undef
+mongoose.connect(process.env.MNGODB_URL || "mongodb://getmycart:aniruddha@getmycart.cluster-c2jyxsybbctr.ap-south-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("connection sucessful")
+}).catch((e) => {
+    console.log("not connected", e)
+})
+
+app.get("/api/config/paypal", (req, res) => {
+    // eslint-disable-next-line no-undef
+    res.send(process.env.PAYPAL_CLIENT_ID || "sb")
+})
+
+
+app.use("/api/product", productRouter)
+
+app.get('/', (req, res) => {
+    res.send("server is ready");
+});
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 5002;
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`)
+    console.log("aniruddha" + process.env.PORT)
+});
