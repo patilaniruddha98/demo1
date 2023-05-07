ed1e601e3debef2c84832331eb0675f404197209
Aniruddha Patil <patilaniruddha98@gmail.com>
2023-05-07 14:00:59 +0530

    6 commit

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.use(cors({
 }));
 
 // eslint-disable-next-line no-undef
-mongoose.connect(process.env.MNGODB_URL || "mongodb://getmycart:aniruddha@getmycart.cluster-c2jyxsybbctr.ap-south-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=./rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", {
-    //tlsCAFile: "./rds-combined-ca-bundle.pem",
+mongoose.connect(process.env.MNGODB_URL || "mongodb://getmycart:aniruddha@getmycart.cluster-c2jyxsybbctr.ap-south-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", {
+    tlsCAFile: "./rds-combined-ca-bundle.pem",
+    tlsAllowInvalidCertificates: true,
+    tlsAllowInvalidHostnames: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
